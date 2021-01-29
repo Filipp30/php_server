d44fd23d9577024e5a1f9fa69cb4db73758b4f30
Filipp Grigoruk <filipp-tts@outlook.com>
2021-01-29 06:13:51 +0100

    .ENV file

--- php_server/components/mailer/Mailer.php
+++ php_server/components/mailer/Mailer.php
@@ -13,16 +13,16 @@ class Mailer{
     public function send_mail($mail_adres,$message_text){
 
             // Create the Transport
-            $transport = (new Swift_SmtpTransport('mail.adm.tools', 2525))
-                ->setUsername('filipp@stuworld.space')
-                ->setPassword('@azerty123')
+            $transport = (new Swift_SmtpTransport($_ENV['MAIL_HOST'], $_ENV['MAIL_PORT']))
+                ->setUsername($_ENV['MAIL_USERNAME'])
+                ->setPassword($_ENV['MAIL_PASSWORD'])
             ;
             // Create the Mailer using your created Transport
             $mailer = new Swift_Mailer($transport);
             //
             // Create a message
             $message = (new Swift_Message('No-reply'))
-                ->setFrom(['filipp@stuworld.space' => 'Admin'])
+                ->setFrom([$_ENV['MAIL_FROM_ADDRESS'] => 'Admin'])
                 ->setTo([$mail_adres => 'Message from Admin. No reply'])
                 ->setBody($message_text);
             // Attach the generated PDF from earlier)
